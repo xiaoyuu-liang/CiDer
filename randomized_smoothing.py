3f04f78095803c048090c1cd1de7412eee667471
GxxxMiii <xiaoyuliang@buaa.edu.cn>
2024-05-14 20:49:34 +0800

    update

---
+++ randomized_smoothing.py
@@ -0,0 +1,191 @@
+from sacred import Experiment
+import seml
+
+ex = Experiment()
+seml.setup_logger(ex)
+
+
+@ex.config
+def config():
+    overwrite = None
+    db_collection = None
+    if db_collection is not None:
+        ex.observers.append(seml.create_mongodb_observer(
+            db_collection, overwrite=overwrite))
+
+    # default params
+    dataset = 'cora'
+    n_per_class = 20
+    seed = 42
+
+    patience = 50
+    max_epochs = 3000
+    lr = 1e-3
+    weight_decay = 1e-3
+
+    model = 'gcn'
+    n_hidden = 64
+    p_dropout = 0.5
+
+    pf_plus_adj = 0.0000
+    pf_minus_adj = 0.0288
+
+    pf_plus_att = 0.0004
+    pf_minus_att = 0.0285
+
+    n_samples_train = 1
+    batch_size_train = 1
+
+    n_samples_pre_eval = 10
+    n_samples_eval = 100
+    batch_size_eval = 10
+
+    mean_softmax = False
+    conf_alpha = 0.01
+    early_stopping = True
+
+    save_dir = 'gnn_checkpoints'
+
+
+@ex.automain
+def run(_config, dataset, n_per_class, seed,
+        patience, max_epochs, lr, weight_decay, model, n_hidden, p_dropout,
+        pf_plus_adj, pf_plus_att, pf_minus_adj, pf_minus_att, conf_alpha,
+        n_samples_train, n_samples_pre_eval, n_samples_eval, mean_softmax, early_stopping,
+        batch_size_train, batch_size_eval, save_dir,
+        ):
+    import numpy as np
+    import torch
+    from src.general_utils import load_and_standardize, get_train_val_test_split, binarize_labels
+    from src.model.classifier import GCN, GAT, APPNP, SAGE
+    from src.model.randomizer.training import train_gnn, train_pytorch
+    from src.model.randomizer.prediction import predict_smooth_gnn, predict_smooth_pytorch
+    from src.model.randomizer.cert import binary_certificate, joint_binary_certificate
+    from src.model.randomizer.utils import (accuracy_majority, sample_batch_pyg)
+    from torch_geometric.data import DataLoader as PyGDataLoader
+    print(_config)
+
+    save_name = f'rand_{model}_{dataset}'
+
+    sample_config = {
+        'n_samples': n_samples_train,
+        'pf_plus_adj': pf_plus_adj,
+        'pf_plus_att': pf_plus_att,
+        'pf_minus_adj': pf_minus_adj,
+        'pf_minus_att': pf_minus_att,
+    }
+
+    # if we need to sample at least once and at least one flip probability is non-zero
+    if n_samples_train > 0 and (pf_plus_adj+pf_plus_att+pf_minus_adj+pf_minus_att > 0):
+        sample_config_train = sample_config
+        sample_config_train['mean_softmax'] = mean_softmax
+    else:
+        sample_config_train = None
+    sample_config_eval = sample_config.copy()
+    sample_config_eval['n_samples'] = n_samples_eval
+    
+    sample_config_pre_eval = sample_config.copy()
+    sample_config_pre_eval['n_samples'] = n_samples_pre_eval
+
+    datafile = f'data/{dataset}.npz'
+    graph = load_and_standardize(datafile)
+
+    edge_idx = torch.LongTensor(
+        np.stack(graph.adj_matrix.nonzero())).cuda()
+    attr_idx = torch.LongTensor(
+        np.stack(graph.attr_matrix.nonzero())).cuda()
+    labels = torch.LongTensor(graph.labels).cuda()
+
+    n, d = graph.attr_matrix.shape
+    nc = graph.labels.max() + 1
+
+    idx = {}
+    bi_labels =  binarize_labels(graph.labels)
+    idx['train'], idx['val'], idx['test'] = get_train_val_test_split(random_state=np.random.RandomState(seed),
+                                                                     labels=bi_labels,
+                                                                     train_examples_per_class=n_per_class,
+                                                                     val_examples_per_class=n_per_class,
+                                                                     test_examples_per_class=None)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if model.lower() == 'gcn':
+        model = GCN(nfeat=graph.num_node_attr, nhid=16, nclass=graph.num_classes, device=device)
+    elif model.lower() == 'gat':
+        model = GAT(nfeat=graph.num_node_attr, nhid=2, heads=8, nclass=graph.num_classes, device=device)
+    elif model.lower() == 'appnp':
+        model = APPNP(nfeat=graph.num_node_attr, nhid=16, K=8, alpha=0.15, nclass=graph.num_classes, device=device)
+    elif model.lower() == 'sage':
+        model = SAGE(nfeat=graph.num_node_attr, nhid=16, nclass=graph.num_classes, device=device)
+    model.to(device)
+
+    print(f'nodes: {n}, features: {d}, classes: {nc}')
+    trace_val = train_gnn(model=model, edge_idx=edge_idx, attr_idx=attr_idx, labels=labels, n=n, d=d, nc=nc,
+                          idx_train=idx['train'], idx_val=idx['val'], lr=lr, weight_decay=weight_decay,
+                          patience=patience, max_epochs=max_epochs, display_step=10,
+                          sample_config=sample_config_train,
+                          batch_size=batch_size_train, early_stopping=early_stopping)
+
+    pre_votes = predict_smooth_gnn(attr_idx=attr_idx, edge_idx=edge_idx,
+                                   sample_config=sample_config_pre_eval,
+                                   model=model, n=n, d=d, nc=nc,
+                                   batch_size=batch_size_eval)
+
+    votes = predict_smooth_gnn(attr_idx=attr_idx, edge_idx=edge_idx,
+                               sample_config=sample_config_eval,
+                               model=model, n=n, d=d, nc=nc,
+                               batch_size=batch_size_eval)
+
+    acc_majority = {}
+    for split_name in ['train', 'val', 'test']:
+        acc_majority[split_name] = accuracy_majority(votes=votes, labels=graph.labels, idx=idx[split_name])
+
+    votes_max = votes.max(1)[idx['test']]
+
+    agreement = (votes.argmax(1) == pre_votes.argmax(1)).mean() 
+
+    # we are perturbing ONLY the ATTRIBUTES
+    if pf_plus_adj == 0 and pf_minus_adj == 0:
+        print('Just ATT')
+        grid_base, grid_lower, grid_upper = binary_certificate(
+            votes=votes, pre_votes=pre_votes, n_samples=n_samples_eval, conf_alpha=conf_alpha,
+            pf_plus=pf_plus_att, pf_minus=pf_minus_att)
+    # we are perturbing ONLY the GRAPH
+    elif pf_plus_att == 0 and pf_minus_att == 0:
+        print('Just ADJ')
+        grid_base, grid_lower, grid_upper = binary_certificate(
+            votes=votes, pre_votes=pre_votes, n_samples=n_samples_eval, conf_alpha=conf_alpha,
+            pf_plus=pf_plus_adj, pf_minus=pf_minus_adj)
+    else:
+        grid_base, grid_lower, grid_upper = joint_binary_certificate(
+            votes=votes, pre_votes=pre_votes, n_samples=n_samples_eval, conf_alpha=conf_alpha,
+            pf_plus_adj=pf_plus_adj, pf_minus_adj=pf_minus_adj,
+            pf_plus_att=pf_plus_att, pf_minus_att=pf_minus_att)
+
+    mean_max_ra_base = (grid_base > 0.5)[:, :, 0].argmin(1).mean()
+    mean_max_rd_base = (grid_base > 0.5)[:, 0, :].argmin(1).mean()
+    mean_max_ra_loup = (grid_lower >= grid_upper)[:, :, 0].argmin(1).mean()
+    mean_max_rd_loup = (grid_lower >= grid_upper)[:, 0, :].argmin(1).mean()
+
+
+    run_id = _config['overwrite']
+    db_collection = _config['db_collection']
+
+    
+    torch.save(model.state_dict(), save_name)
+
+    # the returned result will be written into the database
+    results = {
+        'acc_majority_train': acc_majority['train'],
+        'acc_majority_val': acc_majority['val'],
+        'acc_majority_test': acc_majority['test'],
+        'above_99': (votes_max >= 0.99 * n_samples_eval).mean(),
+        'above_95': (votes_max >= 0.95 * n_samples_eval).mean(),
+        'above_90': (votes_max >= 0.90 * n_samples_eval).mean(),
+        'mean_max_ra_base': mean_max_ra_base,
+        'mean_max_rd_base': mean_max_rd_base,
+        'mean_max_ra_loup': mean_max_ra_loup,
+        'mean_max_rd_loup': mean_max_rd_loup, 
+        'agreement': agreement,
+    }
+
+    return results
---
+++ src/model/diffusion/__init__.py
@@ -0,0 +1,9 @@
+from . import utils
+from . import diffusion_utils
+
+from .diffusion_model import GraphJointDiffuser
+from .train_metrics import *
+from .noise_schedule import *
+from .transformer_model import *
+from .extra_features import *
+
